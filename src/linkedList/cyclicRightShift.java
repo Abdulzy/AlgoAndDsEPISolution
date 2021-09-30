2b05edffb6a47a978d460b73fdd6d98fe2e743a6
abdul <abdul.isegen@gmail.com>
2021-09-30 11:12:12 -0400

    9/30/2021 update

---
+++ src/linkedList/cyclicRightShift.java
@@ -0,0 +1,10 @@
+package linkedList;
+
+public class cyclicRightShift {
+  public static void solution(ListNode<Integer> nodeToDelete) {
+    //iterate through until
+  }
+
+  public static void main(String[] args) {
+  }
+}
