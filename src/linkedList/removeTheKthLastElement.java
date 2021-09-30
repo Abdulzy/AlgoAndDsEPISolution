446253c19e79e0b619cca2618f1d3035180aa62a
abdul <abdul.isegen@gmail.com>
2021-09-30 10:35:37 -0400

    9/30/2021 update

---
+++ src/linkedList/removeTheKthLastElement.java
@@ -0,0 +1,12 @@
+package linkedList;
+
+import scoring.scoring;
+
+public class removeTheKthLastElement {
+  public static void solution(ListNode<Integer> nodeToDelete) {
+
+  }
+
+  public static void main(String[] args) {
+  }
+}
