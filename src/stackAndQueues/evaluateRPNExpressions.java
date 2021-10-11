d1b955022da9265923e3574fc3e06edad70a5d45
abdul <abdul.isegen@gmail.com>
2021-10-11 11:08:47 -0400

    10/11/2021 update

---
+++ src/stackAndQueues/evaluateRPNExpressions.java
@@ -0,0 +1,49 @@
+package stackAndQueues;
+
+import scoring.scoring;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class evaluateRPNExpressions {
+
+  public static int solution(String RPNExpression) {
+    Deque<Integer> intermediateResults = new LinkedList<>();
+    String delimiter = ",";
+    String[] symbols = RPNExpression.split(delimiter);
+    for (String token : symbols) {
+      if (token.length() == 1 && "+-*/".contains(token)){
+      final int y = intermediateResults.removeFirst();
+      final int x = intermediateResults.removeFirst();
+      switch (token.charAt(0)){
+        case '+':
+          intermediateResults.addFirst(x + y);
+          break ;
+        case '-':
+          intermediateResults.addFirst(x - y);
+          break ;
+        case '*':
+          intermediateResults.addFirst(x * y);
+          break ;
+        case '/':
+          intermediateResults.addFirst(x / y);
+          break ;
+        default:
+          throw new IllegalArgumentException("Malformed RPN at :" + token);
+      }
+      } else { // token is a number.
+      intermediateResults.addFirst(Integer.parseInt(token));
+      }
+    }
+  return intermediateResults.removeFirst();
+  }
+
+
+  public static void main(String[] args) {
+    var s = "3,4,+,2,*,1,+";
+
+    System.out.println(solution(s));
+    System.out.println(scoring.scoreSolution(3,3,3,4));
+
+  }
+}
--- src/stackAndQueues/stackWithMaxAPI.java
+++ src/stackAndQueues/stackWithMaxAPI.java
@@ -1,4 +1,64 @@
 package stackAndQueues;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class stackWithMaxAPI {
+
+  private static class MaxWithCount {
+    public Integer max;
+    public Integer count;
+
+    public MaxWithCount(Integer max, Integer count) {
+      this.max = max;
+      this.count = count;
+    }
+  }
+
+  public static class Stack {
+    private final Deque<Integer> element = new LinkedList<>();
+    private final Deque<MaxWithCount> cachedMaxWithCount = new LinkedList<>();
+
+    public boolean empty() {
+      return element.isEmpty();
+    }
+
+    public Integer max() {
+      if (empty()) {
+        throw new IllegalStateException("max(): empty stack");
+      }
+      assert cachedMaxWithCount.peekFirst() != null;
+      return cachedMaxWithCount.peekFirst().max;
+    }
+
+    public Integer pop() {
+      if (empty()) {
+        throw new IllegalStateException("pop(): empty stack");
+      }
+      Integer popElement = element.removeFirst();
+      assert cachedMaxWithCount.peekFirst() != null;
+      if (popElement.equals(cachedMaxWithCount.peekFirst().max)) {
+        cachedMaxWithCount.peekFirst().count
+          = cachedMaxWithCount.peekFirst().count - 1;
+        if (cachedMaxWithCount.peekFirst().count.equals(0)) {
+          cachedMaxWithCount.removeFirst();
+        }
+      }
+      return popElement;
+    }
+
+    public void push(Integer x) {
+      element.addFirst(x);
+      if (!cachedMaxWithCount.isEmpty()) {
+        if (Integer.compare(x, cachedMaxWithCount.peekFirst().max) == 0) {
+          cachedMaxWithCount.peekFirst().count
+            = cachedMaxWithCount.peekFirst().count + 1;
+        } else if (Integer.compare(x, cachedMaxWithCount.peekFirst().max) > 0) {
+          cachedMaxWithCount.addFirst(new MaxWithCount(x, 1));
+        }
+      } else {
+        cachedMaxWithCount.addFirst(new MaxWithCount(x, 1));
+      }
+    }
+  }
 }
