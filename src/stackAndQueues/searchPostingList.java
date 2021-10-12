9051c443fe7295552fa2a9226402aeda9a6738fa
abdul <abdul.isegen@gmail.com>
2021-10-12 09:48:14 -0400

    10/12/2021 update

---
+++ src/stackAndQueues/searchPostingList.java
@@ -0,0 +1,33 @@
+package stackAndQueues;
+
+import scoring.scoring;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class searchPostingList {
+//  public static void solution(PostingListNode L) {
+//    Deque<PostingListNode> s = new LinkedList<>();
+//    int order = ®;
+//    s.addFirst(L);
+//    while (!s.isEmpty()){
+//      PostingListNode curr = s.removeFirst();
+//      if (curr != null && curr.order == -1) {
+//        curr.order = order++;
+//// Stack is last-in, first -out, and we want to process
+//// the jump node first, so push next, then push jump.
+//        s.addFirst(curr.next);
+//        s.addFirst(curr.jump);
+//      }
+//    }
+//  }
+//
+//
+//  public static void main(String[] args) {
+//    var s = "{,(,),[,],}{";
+//
+//    System.out.println(solution(s));
+//    System.out.println(scoring.scoreSolution(5,5,5,5));
+//
+//  }
+}
