b0f3b5eb51ac361068c226b314252d13914631bb
abdul <abdul.isegen@gmail.com>
2021-10-20 09:58:40 -0400

    10/20/2021 update

--- src/Queue/queueWithMaxIntro.java
+++ src/Queue/queueWithMaxIntro.java
@@ -1,4 +1,17 @@
 package Queue;
 
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class queueWithMaxIntro {
+  private Deque<Integer> data = new LinkedList<>();
+  public void enqueue(Integer x) { data.add(x); }
+  public Integer dequeueQ() { return data.removeFirst(); }
+  public Integer max() {
+    if(!data.isEmpty()){
+      return Collections.max(data);
+    }
+    throw new IllegalStateException("Cannot perform max() on empty queue.");
+  }
 }
