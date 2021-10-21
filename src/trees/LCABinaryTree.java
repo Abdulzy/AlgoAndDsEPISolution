6d71e47af9876d7cdb43253f34c9604fefba764d
abdul <abdul.isegen@gmail.com>
2021-10-21 10:44:51 -0400

    10/21/2021 update

---
+++ src/trees/LCABinaryTree.java
@@ -0,0 +1,27 @@
+package trees;
+
+public class LCABinaryTree {
+  TreeNode found = null;
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    recurseTree(root,p,q);
+
+    return this.found;
+  }
+
+  public boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q){
+    if(currentNode == null){
+      return false;
+    }
+    int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+
+    int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+    int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+    if (mid + left + right >= 2) {
+      this.found = currentNode;
+    }
+
+    return (mid + left + right > 0);
+  }
+}
