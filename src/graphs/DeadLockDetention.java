6ca4448b5268327f6d55af494eb4799e2e05fa0c
abdul <abdul.isegen@gmail.com>
2021-11-15 11:31:30 -0500

    11/15/2021 update

--- src/graphs/DeadLockDetention.java
+++ src/graphs/DeadLockDetention.java
@@ -1,4 +1,8 @@
 package graphs;
 
+// mark values grey at the first instance
+// if a gray has been met means there is a cycle
+// makes it black if not of the edges have a cycle
+
 public class DeadLockDetention {
 }
