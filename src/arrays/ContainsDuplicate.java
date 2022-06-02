ba7072c9c47f3593e6e1f37150baaaf488cdecdb
abdul <abdul.isegen@gmail.com>
2022-06-02 12:22:23 -0400

    06/02/2022 update

--- src/arrays/ContainsDuplicate.java
+++ src/arrays/ContainsDuplicate.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
-  public boolean containsDuplicate(int[] nums) {
+  public boolean Solution(int[] nums) {
     Set<Integer> set = new HashSet<>(nums.length);
     for (int x: nums) {
       if (set.contains(x)) return true;
