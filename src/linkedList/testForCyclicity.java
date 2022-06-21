51cdf437621677ef44e27348c19eaf6ab9208e2f
abdul <abdul.isegen@gmail.com>
2022-06-21 11:12:17 -0400

    06/20/2022 update

--- src/linkedList/testForCyclicity.java
+++ src/linkedList/testForCyclicity.java
@@ -8,10 +8,51 @@ public class testForCyclicity {
   public static LinkedList<Integer> solution(LinkedList<Integer> list, int start, int stop) {
 
     var ans = new LinkedList<Integer>();
-    // loop through till values repeat, if  does repeat return head, if it never does return null
+    // loop through and add values to hash map and if value already exist return that value
     return ans;
   }
+  private ListNode getIntersect(ListNode head) {
+    ListNode tortoise = head;
+    ListNode hare = head;
 
+    // A fast pointer will either loop around a cycle and meet the slow
+    // pointer or reach the `null` at the end of a non-cyclic list.
+    while (hare != null && hare.next != null) {
+      tortoise = tortoise.next;
+      hare = hare.next.next;
+      if (tortoise == hare) {
+        return tortoise;
+      }
+    }
+
+    return null;
+  }
+
+  public ListNode detectCycle(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+
+    // If there is a cycle, the fast/slow pointers will intersect at some
+    // node. Otherwise, there is no cycle, so we cannot find an entrance to
+    // a cycle.
+    ListNode intersect = getIntersect(head);
+    if (intersect == null) {
+      return null;
+    }
+
+    // To find the entrance to the cycle, we have two pointers traverse at
+    // the same speed -- one from the front of the list, and the other from
+    // the point of intersection.
+    ListNode ptr1 = head;
+    ListNode ptr2 = intersect;
+    while (ptr1 != ptr2) {
+      ptr1 = ptr1.next;
+      ptr2 = ptr2.next;
+    }
+
+    return ptr1;
+  }
 
   public static void main(String[] args) {
     var s = new LinkedList<Integer>();
