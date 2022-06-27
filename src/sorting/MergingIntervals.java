7e554edf6d9f690610b54867bde1ba911ecf36fa
abdul <abdul.isegen@gmail.com>
2022-06-27 10:47:48 -0400

    06/27/2022 update

--- src/sorting/MergingIntervals.java
+++ src/sorting/MergingIntervals.java
@@ -1,5 +1,26 @@
 package sorting;
 
-//dont get it still
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+
 public class MergingIntervals {
+  public int[][] merge(int[][] intervals) {
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+    LinkedList<int[]> merged = new LinkedList<>();
+    for (int[] interval : intervals) {
+      // if the list of merged intervals is empty or if the current
+      // interval does not overlap with the previous, simply append it.
+      if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+        merged.add(interval);
+      }
+      // otherwise, there is overlap, so we merge the current and previous
+      // intervals.
+      else {
+        merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+      }
+    }
+    return merged.toArray(new int[merged.size()][]);
+  }
 }
