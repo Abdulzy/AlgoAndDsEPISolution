e594de7f4b963b167665d10d661f83c99db29ddb
abdul <abdul.isegen@gmail.com>
2022-07-01 11:17:43 -0400

    7/1/2022 update

---
+++ src/linkedList/ReverseLinkedLise.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+public class ReverseLinkedLise {
+  public ListNode2 reverseList(ListNode2 head) {
+    ListNode2 prev = null;
+    ListNode2 curr = head;
+    while (curr != null) {
+      ListNode2 nextTemp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextTemp;
+    }
+    return prev;
+  }
+}
