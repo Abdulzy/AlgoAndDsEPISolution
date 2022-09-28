03ba7f13d44002557efe113f78ed21a667c149ac
abdul <abdul.isegen@gmail.com>
2022-09-28 10:04:47 -0400

    9/22/2022 update

---
+++ src/trees/PathSum2.java
@@ -0,0 +1,32 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSum2 {
+  List<List<Integer>> list;
+  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+    list  = new ArrayList<>();
+    sum(root,targetSum,new ArrayList<>());
+    return list;
+  }
+  void sum(TreeNode root, int sum, List<Integer> curr){
+    if(root==null) return ;
+    if(isLeaf(root)){
+      if(sum==root.val){
+        curr.add(root.val);
+        list.add(new ArrayList<>(curr));
+        curr.remove(curr.size()-1);
+      }
+      return;
+    }
+    curr.add(root.val);
+    sum(root.left,sum-root.val,curr);
+    sum(root.right,sum-root.val,curr);
+    curr.remove(curr.size()-1);
+  }
+
+  public boolean isLeaf(TreeNode root){
+    return root.left==root.right;
+  }
+}
