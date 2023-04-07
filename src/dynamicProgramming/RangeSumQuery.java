9fe004b7d7ec23812b64a87872452f2824c3ee47
abdul <abdul.isegen@gmail.com>
2023-04-07 10:26:07 -0400

    4/7/2022 update

--- src/dynamicProgramming/RangeSumQuery.java
+++ src/dynamicProgramming/RangeSumQuery.java
@@ -1,11 +1,29 @@
 package dynamicProgramming;
 
-public class RangeSumQuery {
-  public RangeSumQuery(int[] nums) {
+import arrays.DutchNationalFlag;
+import scoring.scoring;
 
+import java.util.Arrays;
+
+public class RangeSumQuery {
+  int[] Arr;
+
+  public RangeSumQuery(int[] nums) {
+    Arr = nums;
   }
 
   public int sumRange(int left, int right) {
+    int result = 0;
+    for (int i = left; i <= right; i++) {
+      result += Arr[i];
+    }
+    return result;
+  }
 
+  public static void main(String[] args) {
+    var s = new RangeSumQuery(new int[]{-2, 0, 3, -5, 2, -1});
+    System.out.println(s.sumRange(0,2));
+    System.out.println(s.sumRange(2,5));
+    System.out.println(s.sumRange(0,5));
   }
 }
