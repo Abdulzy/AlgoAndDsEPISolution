a3a3fae47a89f034003357bd2c9f182b109c8c13
abdul <abdul.isegen@gmail.com>
2023-04-07 11:35:39 -0400

    4/7/2022 update

---
+++ src/dynamicProgramming/HouseRobber.java
@@ -0,0 +1,31 @@
+package dynamicProgramming;
+
+public class HouseRobber {
+  //initial attempt was wrong but good
+  public int rob1(int[] nums) {
+    int odd = 0;
+    int even = 0;
+    for(int i = 0; i < nums.length ; i++){
+      if(i%2 == 0){
+        even += nums[i];
+      }
+      else{
+        odd += nums[i];
+      }
+    }
+
+    return Math.max(odd,even);
+  }
+  public int rob(int[] nums) {
+    int prev1 = 0;
+    int prev2 = 0;
+
+    for (final int num : nums) {
+      final int dp = Math.max(prev1, prev2 + num);
+      prev2 = prev1;
+      prev1 = dp;
+    }
+
+    return prev1;
+  }
+}
