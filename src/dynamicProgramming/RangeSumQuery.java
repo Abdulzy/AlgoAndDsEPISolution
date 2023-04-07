fc2e739b0214fe98deb969a48361255707e4a981
abdul <abdul.isegen@gmail.com>
2023-04-07 10:30:31 -0400

    4/7/2022 update

--- src/dynamicProgramming/RangeSumQuery.java
+++ src/dynamicProgramming/RangeSumQuery.java
@@ -19,6 +19,26 @@ public class RangeSumQuery {
     }
     return result;
   }
+//  Leetcode solution
+//  class NumArray {
+//    int[] n;
+//    public NumArray(int[] nums) {
+//      n=new int[nums.length];
+//      n[0]=nums[0];
+//      for(int i=1;i<nums.length;i++){
+//        n[i]=nums[i]+n[i-1];
+//      }
+//    }
+//
+//    public int sumRange(int left, int right) {
+//      if(left==0){
+//        return n[right];
+//      }
+//      else{
+//        return n[right]-n[left-1];
+//      }
+//    }
+//  }
 
   public static void main(String[] args) {
     var s = new RangeSumQuery(new int[]{-2, 0, 3, -5, 2, -1});
