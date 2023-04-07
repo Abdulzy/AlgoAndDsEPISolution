b34a64435d0a906c00bd4a48874733a154655123
abdul <abdul.isegen@gmail.com>
2023-04-07 11:09:54 -0400

    4/7/2022 update

--- src/dynamicProgramming/CountingBits.java
+++ src/dynamicProgramming/CountingBits.java
@@ -2,4 +2,30 @@ package dynamicProgramming;
 
 public class CountingBits {
   //No idea how it works
+  public static int[] CountingBits(int n) {
+    int []arr=new int[n+1];
+
+    for(int i=0;i<=n;i++)
+    {
+      arr[i]= countBit(i);
+    }
+    return arr;
+  }
+  public static int countBit(int x){
+    int count=0;
+    while(x!=0)
+    {
+      count++;
+      x=x&(x-1);
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    int[] s = CountingBits(3);
+    for (int j: s) {
+      System.out.println(j);
+
+    }
+  }
 }
