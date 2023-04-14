67700d9c3d0f3520cdc09f5a19b3ff2cc46ae02a
abdul <abdul.isegen@gmail.com>
2023-04-13 23:28:01 -0400

    4/13/2022 update

--- src/dynamicProgramming/HouseRobber2.java
+++ src/dynamicProgramming/HouseRobber2.java
@@ -1,4 +1,21 @@
 package dynamicProgramming;
 
+import java.util.Arrays;
+
 public class HouseRobber2 {
+
+  public int rob(int[] nums) {
+    if (nums.length == 1) return nums[0];
+    return Math.max(solve(Arrays.copyOfRange(nums, 1, nums.length)), solve(Arrays.copyOfRange(nums, 0, nums.length - 1)));
+  }
+  public static int solve(int[] nums) {
+    int dp1 = 0, dp2 = 0;
+    for (int i: nums) {
+      int temp = dp1;
+      dp1 = dp2;
+      dp2 = Math.max(temp + i, dp2);
+    }
+    return dp2;
+  }
+
 }
