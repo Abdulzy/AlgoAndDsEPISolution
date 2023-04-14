b70f158c0411a56d5b99155410c4f50cb5130ea7
abdul <abdul.isegen@gmail.com>
2023-04-13 23:29:25 -0400

    4/13/2022 update

--- src/dynamicProgramming/MaximumSubArray.java
+++ src/dynamicProgramming/MaximumSubArray.java
@@ -2,7 +2,7 @@ package dynamicProgramming;
 
 public class MaximumSubArray {
   //Solved it before but want to try a different route
-  public int MaximumSubArray(int[] nums) {
+  public static int MaximumSubArray(int[] nums) {
     int count=0;
     int max=nums[0];
     for(int i=0;i<nums.length;i++){
@@ -15,6 +15,10 @@ public class MaximumSubArray {
       }
     }
     return max;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(MaximumSubArray(new int[]{1,2,-3,5,1}));
   }
 
 }
