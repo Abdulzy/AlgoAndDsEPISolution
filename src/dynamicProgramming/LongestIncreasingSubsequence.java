f85f79c324ddb96fb813d19c365904b060ba6554
abdul <abdul.isegen@gmail.com>
2023-04-15 23:18:09 -0400

    4/14/2022 update

--- src/dynamicProgramming/LongestIncreasingSubsequence.java
+++ src/dynamicProgramming/LongestIncreasingSubsequence.java
@@ -42,4 +42,26 @@ public class LongestIncreasingSubsequence {
   }
 
   //My new approach would be to check if the next value is greater or less and increment the increase counter
+  public int lengthOfLIS2(int[] nums) {
+    int size = nums.length;
+    int[] dp = new int[size];
+    dp[0] = nums[0];
+    int max = 1;
+
+    int j = 0;
+    for(int i = 1; i< size; i++) {
+      if(nums[i] > dp[j]) {
+        j = j+1;
+        dp[j] = nums[i];
+        max++;
+      } else {
+        int ref = j;
+        while(ref>=0 && dp[ref] >= nums[i]) {
+          ref--;
+        }
+        dp[ref+1] = nums[i];
+      }
+    }
+    return max;
+  }
 }
