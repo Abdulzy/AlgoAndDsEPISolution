c998dc756924a394d121fb28cb2565cd62812087
abdul <abdul.isegen@gmail.com>
2023-04-16 23:27:20 -0400

    4/16/2022 update

--- src/dynamicProgramming/LongestPalindrome.java
+++ src/dynamicProgramming/LongestPalindrome.java
@@ -4,5 +4,5 @@ public class LongestPalindrome {
   // solve a coding assessment last week with this
   // although the solution was a longer O(N)
   // Approach for this would be to start from the middle and expand for as long as its a palindrome
-
+  // This only works if the palindrome starts from the center but would fail if it doesn't
 }
