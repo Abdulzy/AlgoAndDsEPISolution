9529890c1c73e773c8816599d2d052babd33159a
abdul <abdul.isegen@gmail.com>
2023-04-19 22:58:38 -0400

    4/19/2022 update

--- src/dynamicProgramming/CombinationSumIV.java
+++ src/dynamicProgramming/CombinationSumIV.java
@@ -3,4 +3,23 @@ package dynamicProgramming;
 public class CombinationSumIV {
   // Had the right approach but couldn't finish appropraitely
   // Went with recursion, couln't think of a way to use a 2D array or 3d array
+  public int combinationSum4(int[] nums, int target) {
+    Integer[] memo = new Integer[target + 1];
+    return recurse(nums, target, memo);
+  }
+
+  public int recurse(int[] nums, int remain, Integer[] memo){
+
+    if(remain < 0) return 0;
+    if(memo[remain] != null) return memo[remain];
+    if(remain == 0) return 1;
+
+    int ans = 0;
+    for(int i = 0; i < nums.length; i++){
+      ans += recurse(nums, remain - nums[i], memo);
+    }
+
+    memo[remain] = ans;
+    return memo[remain];
+  }
 }
