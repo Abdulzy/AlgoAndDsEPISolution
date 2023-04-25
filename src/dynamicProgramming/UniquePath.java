082ded8b79e236e01c68f155abe78d9a6d7104ed
abdul <abdul.isegen@gmail.com>
2023-04-25 16:28:58 -0400

    4/25/2022 update

--- src/dynamicProgramming/UniquePath.java
+++ src/dynamicProgramming/UniquePath.java
@@ -5,4 +5,19 @@ public class UniquePath {
   // using DP, initial all the [0][0....n] with 1 and also [0....n][0] with 1
   // aAfter which i would iterate through from[1][1] to the remaining index by adding the values from the left index
   // and top index and inserting it into the current index
+  public int uniquePaths(int m, int n) {
+    int[][] arr = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      arr[i][0] = 1;
+    }
+    for (int j = 0; j < n; j++) {
+      arr[0][j] = 1;
+    }
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        arr[i][j] = arr[i-1][j] + arr[i][j-1];
+      }
+    }
+    return arr[m-1][n-1];
+  }
 }
