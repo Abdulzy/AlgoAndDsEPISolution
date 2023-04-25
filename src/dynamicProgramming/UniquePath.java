627267fd0be7741c56b1596627345575d69d31ab
abdul <abdul.isegen@gmail.com>
2023-04-25 16:20:44 -0400

    4/25/2022 update

--- src/dynamicProgramming/UniquePath.java
+++ src/dynamicProgramming/UniquePath.java
@@ -2,4 +2,7 @@ package dynamicProgramming;
 
 public class UniquePath {
   //all the unique paths from the top leftmost index of the array to the bottom rightmost index of the array
+  // using DP, initial all the [0][0....n] with 1 and also [0....n][0] with 1
+  // aAfter which i would iterate through from[1][1] to the remaining index by adding the values from the left index
+  // and top index and inserting it into the current index
 }
