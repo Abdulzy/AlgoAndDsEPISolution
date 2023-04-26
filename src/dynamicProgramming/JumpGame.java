19841f4f5741abbda6a9bc06a4d1488e2c0abb3c
abdul <abdul.isegen@gmail.com>
2023-04-26 14:32:44 -0400

    4/26/2022 update

--- src/dynamicProgramming/JumpGame.java
+++ src/dynamicProgramming/JumpGame.java
@@ -2,4 +2,8 @@ package dynamicProgramming;
 
 public class JumpGame {
   //Starting from the index 0, find out if you can reach the end by using the values of each index to jump
+  // Using DP make an array of the same length as the array passed in, initiate the array with a max/high value
+  // which would help with decided what the new value would be.
+  // using the value at a index to calculate which other index you can reach. My solution can also account for which
+  // path was taken
 }
