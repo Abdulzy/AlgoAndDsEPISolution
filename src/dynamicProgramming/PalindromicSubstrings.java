844727af971b74cccee0a7cf1cf43b3f976c22f6
abdul <abdul.isegen@gmail.com>
2023-04-27 12:53:57 -0400

    4/27/2022 update

--- src/dynamicProgramming/PalindromicSubstrings.java
+++ src/dynamicProgramming/PalindromicSubstrings.java
@@ -4,5 +4,7 @@ public class PalindromicSubstrings {
   //Finding all the palindrome in the string,for example in "abc" there are 3 palindromes which are a,b,c
   // for "aaa" there are 6 palindromes which are "a", "a", "a", "aa", "aa", "aaa"
   //My thought is to have a for loop that iterates through the string, adds the current char as a palindrome then check
-  // the chars after to see if the substring is a palindrome
+  // the chars after to see if the substring is a palindrome. which would be brute force O(n^3)
+  // Another way would be instead of checking if the substring is a palindrome you use two pointers to
+  // iterate through to find the palindrome
 }
