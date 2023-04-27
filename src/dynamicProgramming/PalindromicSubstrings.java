733ff7a5144c005ae46d30217d05e0d70f4f189e
abdul <abdul.isegen@gmail.com>
2023-04-27 11:19:51 -0400

    4/27/2022 update

--- src/dynamicProgramming/PalindromicSubstrings.java
+++ src/dynamicProgramming/PalindromicSubstrings.java
@@ -1,5 +1,6 @@
 package dynamicProgramming;
 
 public class PalindromicSubstrings {
-
+  //Finding all the palindrome in the string,for example in "abc" there are 3 palindromes which are a,b,c
+  // for "aaa" there are 6 palindromes which are "a", "a", "a", "aa", "aa", "aaa"
 }
