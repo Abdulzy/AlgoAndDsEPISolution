ff9128e09655eda6f8f0f398710add3f2c2264f1
abdul <abdul.isegen@gmail.com>
2023-04-27 12:54:23 -0400

    4/27/2022 update

--- src/dynamicProgramming/PalindromicSubstrings.java
+++ src/dynamicProgramming/PalindromicSubstrings.java
@@ -7,4 +7,27 @@ public class PalindromicSubstrings {
   // the chars after to see if the substring is a palindrome. which would be brute force O(n^3)
   // Another way would be instead of checking if the substring is a palindrome you use two pointers to
   // iterate through to find the palindrome
+  public static int countSubstrings(String s) {
+    if(s.length() == 0) return 0;
+    int odd = 0;
+    int even = 0;
+    for(int k = 0; k < s.length(); k++)
+    {
+      odd += checking(s, k, k);
+      even += checking(s, k, k + 1);
+    }
+    return odd + even;
+  }
+
+  public static int checking(String s, int left, int right)
+  {
+    int count = 0;
+    while(left <= right && left >= 0 && right < s.length() &&  s.charAt(left) == s.charAt(right))
+    {
+      count++;
+      left--;
+      right++;
+    }
+    return count;
+  }
 }
