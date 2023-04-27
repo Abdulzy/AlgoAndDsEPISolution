4141ba27e466a6e776abbbe3fa055f0ca75bf151
abdul <abdul.isegen@gmail.com>
2023-04-27 11:35:51 -0400

    4/27/2022 update

--- src/dynamicProgramming/PalindromicSubstrings.java
+++ src/dynamicProgramming/PalindromicSubstrings.java
@@ -3,4 +3,6 @@ package dynamicProgramming;
 public class PalindromicSubstrings {
   //Finding all the palindrome in the string,for example in "abc" there are 3 palindromes which are a,b,c
   // for "aaa" there are 6 palindromes which are "a", "a", "a", "aa", "aa", "aaa"
+  //My thought is to have a for loop that iterates through the string, adds the current char as a palindrome then check
+  // the chars after to see if the substring is a palindrome
 }
