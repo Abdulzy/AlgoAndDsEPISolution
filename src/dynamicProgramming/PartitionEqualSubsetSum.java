8d8c29fc2d4492f7d5795c8aa3591884271d3821
abdul <abdul.isegen@gmail.com>
2023-05-01 18:26:18 -0400

    5/01/2022 update

---
+++ src/dynamicProgramming/PartitionEqualSubsetSum.java
@@ -0,0 +1,7 @@
+package dynamicProgramming;
+
+public class PartitionEqualSubsetSum {
+  //segment the array into 2 separate arrays that are equal in value
+  // get the total of the whole array, divide the value in 2 then iterate through a 1d array checking all the possible
+  // changes of subtracting each individual value to get the sum
+}
