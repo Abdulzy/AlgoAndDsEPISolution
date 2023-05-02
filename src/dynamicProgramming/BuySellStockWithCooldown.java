5a73ee0c17d637c6800b96418c607c69339df545
abdul <abdul.isegen@gmail.com>
2023-05-02 12:53:57 -0400

    5/02/2022 update

--- src/dynamicProgramming/BuySellStockWithCooldown.java
+++ src/dynamicProgramming/BuySellStockWithCooldown.java
@@ -5,4 +5,32 @@ public class BuySellStockWithCooldown {
   //Find the maximum profit you can achieve. You may complete as many transactions as you like
   // (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
   //After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+
+  public static int maxProfit(int[] prices) {
+    if (prices == null || prices.length < 1) {
+      return 0;
+    }
+
+    int length = prices.length;
+    // buy[i]: max profit if the first "i" days end with a "buy" day
+    int[] buy = new int[length + 1];
+    // buy[i]: max profit if the first "i" days end with a "sell" day
+    int[] sell = new int[length + 1];
+
+    buy[1] = -prices[0];
+
+    for (int i = 2; i <= length; i++) {
+      int price = prices[i - 1];
+      buy[i] = Math.max(buy[i - 1], sell[i - 2] - price);
+      sell[i] = Math.max(sell[i - 1], buy[i - 1] + price);
+    }
+
+    // sell[length] >= buy[length]
+    return sell[length];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(maxProfit(new int[]{1}));
+  }
+
 }
