96125842cf3071efc37babcaf4151c48428cb760
abdul <abdul.isegen@gmail.com>
2023-05-12 23:53:28 -0400

    5/12/2022 update

--- src/graphs/SequenceReconstruction.java
+++ src/graphs/SequenceReconstruction.java
@@ -6,4 +6,7 @@ public class SequenceReconstruction {
   // Check if nums is the shortest possible and the only supersequence. The shortest supersequence is a sequence with
   // the shortest length and has all sequences[i] as subsequences. There could be multiple valid supersequences for the
   // given array sequences.
+  //For example, for sequences = [[1,2],[1,3]], there are two shortest supersequences, [1,2,3] and [1,3,2].
+  //While for sequences = [[1,2],[1,3],[1,2,3]], the only shortest supersequence possible is [1,2,3]. [1,2,3,4] is a
+  // possible supersequence but not the shortest.
 }
