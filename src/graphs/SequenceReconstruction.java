5fe4760d26070f2a8dc975e599e89bf5f4e8b20a
abdul <abdul.isegen@gmail.com>
2023-05-12 23:53:20 -0400

    5/12/2022 update

--- src/graphs/SequenceReconstruction.java
+++ src/graphs/SequenceReconstruction.java
@@ -3,4 +3,7 @@ package graphs;
 public class SequenceReconstruction {
   //You are given an integer array nums of length n where nums is a permutation of the integers in the range [1, n].
   // You are also given a 2D integer array sequences where sequences[i] is a subsequence of nums.
+  // Check if nums is the shortest possible and the only supersequence. The shortest supersequence is a sequence with
+  // the shortest length and has all sequences[i] as subsequences. There could be multiple valid supersequences for the
+  // given array sequences.
 }
