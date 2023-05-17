b5b476c84c4c4eb388ce269b6099db7670e10fbd
abdul <abdul.isegen@gmail.com>
2023-05-17 10:46:46 -0400

    5/17/2023

---
+++ src/arrays/MissingNumber.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class MissingNumber {
+  // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range
+  // that is missing from the array.
+
+  // I'm having multiple thoughts for this:
+  // 1
+  // make a new array(result) of the same length + 1, put true in result[i] when you see i, return the i that doesn't
+  // have true
+}
