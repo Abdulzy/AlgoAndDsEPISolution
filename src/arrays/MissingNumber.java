2dd0722d49b731fb7c50adc7639067c0c416985a
abdul <abdul.isegen@gmail.com>
2023-05-17 10:48:24 -0400

    5/17/2023

--- src/arrays/MissingNumber.java
+++ src/arrays/MissingNumber.java
@@ -8,4 +8,8 @@ public class MissingNumber {
   // 1
   // make a new array(result) of the same length + 1, put true in result[i] when you see i, return the i that doesn't
   // have true
+
+  // 2
+  // get the total of all numbers from 0...n, subtract the values in the provided array from nums then return the final
+  // number. saves space compared to teh first solution but same time.
 }
