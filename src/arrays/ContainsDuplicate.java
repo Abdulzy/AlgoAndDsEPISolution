c60df5918df37f98d6cd2d14f2e974d93ce1859e
abdul <abdul.isegen@gmail.com>
2023-05-17 10:41:23 -0400

    5/17/2023

--- src/arrays/ContainsDuplicate.java
+++ src/arrays/ContainsDuplicate.java
@@ -1,9 +1,22 @@
 package arrays;
 
+import java.util.HashSet;
+
 public class ContainsDuplicate {
   // Given an integer array nums, return true if any value appears at least twice in the array, and
   // return false if every element is distinct.
 
   // Using hashset to store numbers and check if it already exists.
-
+  public boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> numbers = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      if(numbers.contains(nums[i])){
+        return true;
+      }
+      else{
+        numbers.add(nums[i]);
+      }
+    }
+    return false;
+  }
 }
