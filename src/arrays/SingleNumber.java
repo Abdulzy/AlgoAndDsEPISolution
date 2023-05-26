f639e87d5102fd1baf8439f1448207654d9608a5
abdul <abdul.isegen@gmail.com>
2023-05-26 18:33:02 -0400

    5/26/2023

--- src/arrays/SingleNumber.java
+++ src/arrays/SingleNumber.java
@@ -4,4 +4,6 @@ public class SingleNumber {
   //Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
   //
   //You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+  // sort the array, check if the next value is equal to the current one
 }
