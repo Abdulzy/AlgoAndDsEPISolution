70964642c912ddbe164537c41fb9de4f24c7b6ea
abdul <abdul.isegen@gmail.com>
2023-05-25 21:44:08 -0400

    5/25/2023

--- src/arrays/AllMissingNumbers.java
+++ src/arrays/AllMissingNumbers.java
@@ -3,4 +3,7 @@ package arrays;
 public class AllMissingNumbers {
   //Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the
   // range [1, n] that do not appear in nums.
+
+  //Similar to the Missing number question, all i have to do is just add teh values that don't exist into the array.
+
 }
