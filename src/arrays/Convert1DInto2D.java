28a93737f13277eb62414be84f6f1628c2d12ca3
abdul <abdul.isegen@gmail.com>
2023-05-27 12:16:58 -0400

    5/27/2023

--- src/arrays/Convert1DInto2D.java
+++ src/arrays/Convert1DInto2D.java
@@ -1,4 +1,6 @@
 package arrays;
 
 public class Convert1DInto2D {
+  //You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with
+  // creating a 2-dimensional (2D) array with  m rows and n columns using all the elements from original.
 }
