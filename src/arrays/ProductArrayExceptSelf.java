2a9030671916bfa545cc35f091dd280ec3a518f3
abdul <abdul.isegen@gmail.com>
2023-05-28 13:03:26 -0400

    5/28/2023

--- src/arrays/ProductArrayExceptSelf.java
+++ src/arrays/ProductArrayExceptSelf.java
@@ -5,4 +5,8 @@ public class ProductArrayExceptSelf {
   // of nums except nums[i].
   //The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
   //You must write an algorithm that runs in O(n) time and without using the division operation.
+
+  //idea is to go through the array multiplying all values, keep a count for the amount of zeros, if a zero is seen dont
+  // multiply with the rest and save that value with a different object.
+
 }
