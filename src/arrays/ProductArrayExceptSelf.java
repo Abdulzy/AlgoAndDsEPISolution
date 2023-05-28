c1dceebb4e6fa2337ab7396ea7a4b55cf4f2cdf5
abdul <abdul.isegen@gmail.com>
2023-05-28 13:37:01 -0400

    5/28/2023

--- src/arrays/ProductArrayExceptSelf.java
+++ src/arrays/ProductArrayExceptSelf.java
@@ -9,4 +9,37 @@ public class ProductArrayExceptSelf {
   //idea is to go through the array multiplying all values, keep a count for the amount of zeros, if a zero is seen dont
   // multiply with the rest and save that value with a different object.
 
+  public int[] productExceptSelf(int[] nums) {
+    int[] result = new int[nums.length];
+    int totalZero = 0;
+    int fullProduct = 1;
+    for (int i = 0; i < nums.length; i++) {
+      if(nums[i] == 0){
+        totalZero++;
+      }
+      else{
+        fullProduct = nums[i] * fullProduct;
+      }
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if(nums[i] != 0){
+        if(totalZero == 0 ){
+          result[i] = fullProduct / nums[i];
+        }
+        else{
+          result[i] = 0;
+        }
+      }
+      else {
+        if (totalZero > 1){
+          result[i] = 0;
+        }
+        else{
+          result[i] = fullProduct;
+        }
+      }
+    }
+
+    return result;
+  }
 }
