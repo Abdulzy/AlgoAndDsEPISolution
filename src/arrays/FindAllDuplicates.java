e25cf4206ebed9adbadc54b680dd8671d8c03009
abdul <abdul.isegen@gmail.com>
2023-06-02 23:14:59 -0400

    6/02/2023

--- src/arrays/FindAllDuplicates.java
+++ src/arrays/FindAllDuplicates.java
@@ -1,8 +1,28 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class FindAllDuplicates {
   //Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer
   // appears once or twice, return an array of all the integers that appears twice.
   //
   //You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+  public List<Integer> findDuplicates(int[] nums) {
+    List<Integer> ans = new ArrayList<>();
+    Set<Integer> seen = new HashSet<>();
+
+    for (int num : nums) {
+      if (seen.contains(num)) {
+        ans.add(num);
+      } else {
+        seen.add(num);
+      }
+    }
+
+    return ans;
+  }
 }
