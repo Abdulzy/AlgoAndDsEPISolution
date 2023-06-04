d22caf3939e10fef671c071b2898117bd479681f
abdul <abdul.isegen@gmail.com>
2023-06-04 17:56:49 -0400

    6/04/2023

--- src/arrays/SpiralMatrix.java
+++ src/arrays/SpiralMatrix.java
@@ -2,4 +2,7 @@ package arrays;
 
 public class SpiralMatrix {
   //Given an m x n matrix, return all elements of the matrix in spiral order.
+
+  //My idea is to go through the outer layer of the matrix, then I send a new parameter of the smaller
+  // array into a recursive method.
 }
