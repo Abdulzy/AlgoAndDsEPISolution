6e2f84f894539ca863388ba9a1de16042cf276a8
abdul <abdul.isegen@gmail.com>
2023-06-03 21:42:45 -0400

    6/03/2023

--- src/arrays/SetMatrixZeroes.java
+++ src/arrays/SetMatrixZeroes.java
@@ -1,4 +1,12 @@
 package arrays;
 
 public class SetMatrixZeroes {
+
+  //Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+  //
+  //You must do it in place.
+
+  // store the zero locations then make values that match the row and column zero
+
+
 }
