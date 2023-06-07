bc533a0401de8d99fe7c1dcf3617cc8a4bff670c
abdul <abdul.isegen@gmail.com>
2023-06-06 20:40:02 -0400

    6/06/2023

--- src/arrays/RotateImage.java
+++ src/arrays/RotateImage.java
@@ -5,4 +5,7 @@ public class RotateImage {
   //
   //You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
   // DO NOT allocate another 2D matrix and do the rotation.
+
+  // My idea is to solve it with a new 2D array,
+  // change the values of arr[i][j] -> newArr[arr[0].length - 1 - j][arr.length - 1 - i]
 }
