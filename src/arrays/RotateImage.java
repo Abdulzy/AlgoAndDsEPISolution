f3b85e499386af894025bab9c18b87a3099aea02
abdul <abdul.isegen@gmail.com>
2023-06-06 20:58:44 -0400

    6/06/2023

--- src/arrays/RotateImage.java
+++ src/arrays/RotateImage.java
@@ -17,8 +17,19 @@ public class RotateImage {
         result[j][matrix.length - 1 - i] = matrix[i][j];
       }
     }
+  }
 
-    System.out.println(result.toString());
-    System.out.println(matrix.toString());
+  // To do it in one place, to modify all rotational sides at once:
+  public void rotate1(int[][] matrix) {
+    int n = matrix.length;
+    for (int i = 0; i < (n + 1) / 2; i ++) {
+      for (int j = 0; j < n / 2; j++) {
+        int temp = matrix[n - 1 - j][i];
+        matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+        matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+        matrix[j][n - 1 - i] = matrix[i][j];
+        matrix[i][j] = temp;
+      }
+    }
   }
 }
