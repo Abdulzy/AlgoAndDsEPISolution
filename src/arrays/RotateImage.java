1650122d0b9ce20795c167ef6ed3b69f269356d5
abdul <abdul.isegen@gmail.com>
2023-06-06 20:49:58 -0400

    6/06/2023

--- src/arrays/RotateImage.java
+++ src/arrays/RotateImage.java
@@ -8,4 +8,17 @@ public class RotateImage {
 
   // My idea is to solve it with a new 2D array,
   // change the values of arr[i][j] -> newArr[arr[0].length - 1 - j][arr.length - 1 - i]
+
+
+  public void rotate(int[][] matrix) {
+    int[][] result = new int[matrix[0].length][matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        result[j][matrix.length - 1 - i] = matrix[i][j];
+      }
+    }
+
+    System.out.println(result.toString());
+    System.out.println(matrix.toString());
+  }
 }
