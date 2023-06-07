b70ebf550ab8c2de1939fb144eb2a5dccce5b545
abdul <abdul.isegen@gmail.com>
2023-06-07 10:08:53 -0400

    6/07/2023

--- src/arrays/LongestConsecutiveSequence.java
+++ src/arrays/LongestConsecutiveSequence.java
@@ -3,4 +3,7 @@ package arrays;
 public class LongestConsecutiveSequence {
   //Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
   //You must write an algorithm that runs in O(n) time.
+
+  // Solving without the O(n) time would be using Array.sort() then the longest sequence can be gotten
+
 }
