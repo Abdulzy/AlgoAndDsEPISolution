9c7ce075789e0c5ceb1d47eb5b9df09a332b1349
abdul <abdul.isegen@gmail.com>
2023-06-09 15:13:53 -0400

    6/09/2023

--- src/arrays/FirstMissingPositive.java
+++ src/arrays/FirstMissingPositive.java
@@ -4,4 +4,9 @@ public class FirstMissingPositive {
   //Given an unsorted integer array nums, return the smallest missing positive integer.
   //
   //You must implement an algorithm that runs in O(n) time and uses constant extra space.
+
+  // seems rather straight forward but i might be wrong.
+  // 1. For O(nLogn) sort then return the missing number which is easy and constant space.
+  // 2. iterate through the array add values to hashset while keeping a tab on the highest number,
+  // iterate from 1 till the highest number to find the lowest missing number.should be O(n)/O(n^2) and also more space
 }
