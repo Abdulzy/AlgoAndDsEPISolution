ff8b741877f63012aa961647408f547733f0264f
abdul <abdul.isegen@gmail.com>
2023-06-09 15:20:09 -0400

    6/09/2023

--- src/arrays/FirstMissingPositive.java
+++ src/arrays/FirstMissingPositive.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.HashSet;
+
 public class FirstMissingPositive {
   //Given an unsorted integer array nums, return the smallest missing positive integer.
   //
@@ -9,4 +11,21 @@ public class FirstMissingPositive {
   // 1. For O(nLogn) sort then return the missing number which is easy and constant space.
   // 2. iterate through the array add values to hashset while keeping a tab on the highest number,
   // iterate from 1 till the highest number to find the lowest missing number.should be O(n)/O(n^2) and also more space
+
+  public int firstMissingPositive(int[] nums) {
+    HashSet<Integer> numbers = new HashSet<>();
+    int maxNumber = 0;
+    for (int i: nums) {
+      numbers.add(i);
+      maxNumber = Math.max(maxNumber, i);
+    }
+
+    for (int i = 1; i <= maxNumber ; i++) {
+      if(!numbers.contains(i)){
+        return i;
+      }
+    }
+
+    return maxNumber + 1;
+  }
 }
