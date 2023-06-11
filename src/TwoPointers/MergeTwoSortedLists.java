34a283c91b47a84057f9c94abf74232e2fbbd751
abdul <abdul.isegen@gmail.com>
2023-06-11 08:05:01 -0400

    6/11/2023

--- src/TwoPointers/MergeTwoSortedLists.java
+++ src/TwoPointers/MergeTwoSortedLists.java
@@ -6,4 +6,7 @@ public class MergeTwoSortedLists {
   //Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
   //
   //Return the head of the merged linked list.
+
+  //make a new list, using a while loop that goes on till on of the list has to end, compare the currrent values of the
+  // each list, add the lower value to the new list.
 }
