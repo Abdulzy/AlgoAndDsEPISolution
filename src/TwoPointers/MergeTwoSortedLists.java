df072509793ddbe925e82e3371f097da12c110c0
abdul <abdul.isegen@gmail.com>
2023-06-11 08:16:32 -0400

    6/11/2023

--- src/TwoPointers/MergeTwoSortedLists.java
+++ src/TwoPointers/MergeTwoSortedLists.java
@@ -9,4 +9,44 @@ public class MergeTwoSortedLists {
 
   //make a new list, using a while loop that goes on till on of the list has to end, compare the currrent values of the
   // each list, add the lower value to the new list.
+
+  public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    ListNode result = new ListNode();
+
+    ListNode head = result;
+    while(list1.next != null && list2.next != null){
+      if(list1.val > list2.val){
+        result.next = list2;
+        result = result.next;
+        list2 = list2.next;
+      }
+      else{
+        result.next = list1;
+        result = result.next;
+        list1 = list1.next;
+      }
+    }
+    while(list1.next != null){
+      result.next = list1;
+      result = result.next;
+      list1 = list1.next;
+
+    }
+    while(list2.next != null){
+      result.next = list1;
+      result = result.next;
+      list2 = list2.next;
+
+    }
+
+    return head.next;
+  }
 }
