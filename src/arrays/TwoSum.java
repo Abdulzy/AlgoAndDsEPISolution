973e0dcfb48441685406d76a5dd1bb0725e6a0d1
abdul <abdul.isegen@gmail.com>
2023-06-12 11:18:16 -0400

    6/12/2023

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,5 +1,8 @@
 package arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
   //Given an array of integers nums and an integer target,
   // return indices of the two numbers such that they add up to target.
@@ -7,4 +10,9 @@ public class TwoSum {
   //You may assume that each input would have exactly one solution, and you may not use the same element twice.
   //
   //You can return the answer in any order.
+
+
+  //Go through the array and add the values as a key and the index as the value.
+  // go through the array again and find the value difference
+
 }
