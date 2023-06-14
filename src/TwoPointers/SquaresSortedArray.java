79725bbdb711d0ff45ac820ebcc26cc0fc57d411
abdul <abdul.isegen@gmail.com>
2023-06-13 20:53:07 -0400

    6/13/2023

--- src/TwoPointers/SquaresSortedArray.java
+++ src/TwoPointers/SquaresSortedArray.java
@@ -3,4 +3,7 @@ package TwoPointers;
 public class SquaresSortedArray {
   //Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted
   // in non-decreasing order.
+
+  //go through the array squaring each value with itself in the first iteration.
+  // after that you can sort however you please
 }
