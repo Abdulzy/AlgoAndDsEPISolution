1b17ce06cfddd113946ba2b321e174368deb79c7
abdul <abdul.isegen@gmail.com>
2023-06-13 20:56:18 -0400

    6/13/2023

--- src/TwoPointers/SquaresSortedArray.java
+++ src/TwoPointers/SquaresSortedArray.java
@@ -8,4 +8,24 @@ public class SquaresSortedArray {
   // after that you can sort however you please
   // didnt realise it was sorted to begin with, you can compare the absolute value of the numbers and
   // insert into a new array
+
+  public int[] sortedSquares(int[] nums) {
+    int n = nums.length;
+    int[] result = new int[n];
+    int left = 0;
+    int right = n - 1;
+
+    for (int i = n - 1; i >= 0; i--) {
+      int square;
+      if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+        square = nums[right];
+        right--;
+      } else {
+        square = nums[left];
+        left++;
+      }
+      result[i] = square * square;
+    }
+    return result;
+  }
 }
